aac567215de4c2827fd4bcbd9e311abc2d78838e
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-28 02:15:02 -0700

    Replacing with regular expressions

--- regex-working.py
+++ regex-working.py
@@ -1,12 +1,25 @@
+# import re
+
+# def main():
+# 	fh = open("comment.py")
+# 	for line in fh:
+# 		match = re.search('(def|a)n', line)
+# 		if match:
+
+# 			# print(line, end='')
+# 			print(match.group())
+
+# if __name__ == "__main__": main()
+
 import re
 
 def main():
 	fh = open("comment.py")
 	for line in fh:
+		# print(re.sub('(def|a)n','###', line), end='')
 		match = re.search('(def|a)n', line)
 		if match:
+			print(line.replace(match.group(), "amin"), end='')
 
-			# print(line, end='')
-			print(match.group())
-
+		
 if __name__ == "__main__": main()
