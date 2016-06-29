5d250845e1c60fc20d46b5c8c3e4e665f75e5673
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-29 00:26:45 -0700

    Using list of argument

--- function-working.py
+++ function-working.py
@@ -1,8 +1,18 @@
-def main():
-	testfunc(42)
+# def main():
+# 	testfunc(42)
 
-def testfunc(number, another="fas", onemore=43):
-	print('This is a test funciton', number, another, onemore)
+# def testfunc(number, another="fas", onemore=43):
+# 	print('This is a test funciton', number, another, onemore)
+# 	# pass
+
+# if __name__ == "__main__": main()
+
+def main():
+	testfunc(1, 2, 4, 43, 45, 46)
+
+def testfunc(this, that, other, *args):
+	print(this, that, other, args)
+	for n in args: print(n, end=' ')
 	# pass
 
 if __name__ == "__main__": main()
