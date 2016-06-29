51c64b555365e2bcd7a893a901f37190a2dad18c
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-29 00:21:37 -0700

    Defining function

---
+++ function-working.py
@@ -0,0 +1,8 @@
+def main():
+	testfunc(42)
+
+def testfunc(number, another="fas", onemore=43):
+	print('This is a test funciton', number, another, onemore)
+	# pass
+
+if __name__ == "__main__": main()
