732f0e26583128a96cea91fb1b9942f06e2daec2
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-29 00:31:15 -0700

    Using named argument

--- function-working.py
+++ function-working.py
@@ -7,12 +7,21 @@
 
 # if __name__ == "__main__": main()
 
-def main():
-	testfunc(1, 2, 4, 43, 45, 46)
+# def main():
+# 	testfunc(1, 2, 4, 43, 45, 46)
 
-def testfunc(this, that, other, *args):
-	print(this, that, other, args)
-	for n in args: print(n, end=' ')
-	# pass
+# def testfunc(this, that, other, *args):
+# 	print(this, that, other, args)
+# 	for n in args: print(n, end=' ')
+# 	# pass
+
+# if __name__ == "__main__": main()
+
+def main():
+	testfunc(one = 1, two = 2, four = 42)
+
+def testfunc(**kwargs):
+	print("This is a test function", kwargs['one'], kwargs['two'], kwargs['four'])
+	for k in kwargs: print(k, kwargs[k])
 
 if __name__ == "__main__": main()
