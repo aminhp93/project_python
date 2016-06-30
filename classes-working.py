a8a8c2502cd8571d87a00c4c750980b5dc7ffc67
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-30 00:44:53 -0700

    Understanding inheritance

--- classes-working.py
+++ classes-working.py
@@ -38,25 +38,48 @@
 
 # if __name__ == "__main__": main()
 
-class Duck:
-	def __init__(self, **kwargs):
-		self.variables = kwargs
+# class Duck:
+# 	def __init__(self, **kwargs):
+# 		self.variables = kwargs
 
-	def set_variables(self, k, v):
-		self.variables[k] = v
+# 	def set_variables(self, k, v):
+# 		self.variables[k] = v
 
-	def get_variables(self, k):
-		return  self.variables.get(k, None)
+# 	def get_variables(self, k):
+# 		return  self.variables.get(k, None)
+
+# def main():
+# 	donald = Duck(feet = 2)
+# 	donald.set_variables('color', 'blue')
+# 	print(donald.get_variables('feet'))
+# 	print(donald.get_variables('color'))
+
+# if __name__ == "__main__": main()
+
+
+class Animal:
+	def talk(self): print('I have something to say')
+	def walk(self): print('Hey I am walking here')
+	def clothes(self): print('I have nice blothes')
+
+class Duck(Animal):
+	def walk(self):
+		super().walk()
+		print('qfsadf')
+
+class Dog(Animal):
+	def clothes(self):
+		super().clothes()
+		print('I have brown and white fur')
 
 def main():
-	donald = Duck(feet = 2)
-	donald.set_variables('color', 'blue')
-	print(donald.get_variables('feet'))
-	print(donald.get_variables('color'))
+	donald = Duck()
+	donald.walk()
+
+	alo = Dog()
+	alo.clothes()
 
 if __name__ == "__main__": main()
-
-
 
 
 
